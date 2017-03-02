e48d6103d6e454d14627e9792c360c28ffd5230d
George Mathew <george.meg91@gmail.com>
2017-03-02 14:42:22 -0500

    Predicting conferences

--- classify/predict.py
+++ classify/predict.py
@@ -7,9 +7,13 @@ from network.mine import Miner, cite_graph
 from sklearn.feature_extraction import text
 from sklearn.model_selection import StratifiedKFold
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_selection import SelectKBest, chi2
 import sklearn.metrics as sk_metrics
 import numpy as np
 import lda
@@ -62,17 +66,19 @@ TOPIC_THRESHOLD = 3
 DELIMITER = '$|$'
 STOP_WORDS = text.ENGLISH_STOP_WORDS.union(['software', 'engineering'])
 TOKEN_PATTERN = r"(?u)\b\w\w\w+\b"
+K_BEST_RATE = 0.2
 
 
 class Metrics(O):
   EPS = 0.00000001
 
-  def __init__(self, predicted, actual):
+  def __init__(self, predicted=None, actual=None):
     O.__init__(self)
-    self.accuracy = sk_metrics.accuracy_score(actual, predicted)
-    self.precision = sk_metrics.precision_score(actual, predicted, average='weighted')
-    self.recall = sk_metrics.recall_score(actual, predicted, average='weighted')
-    self.f_score = sk_metrics.f1_score(actual, predicted, average='weighted')
+    if predicted is not None and actual is not None:
+      self.accuracy = sk_metrics.accuracy_score(actual, predicted)
+      self.precision = sk_metrics.precision_score(actual, predicted, average='weighted')
+      self.recall = sk_metrics.recall_score(actual, predicted, average='weighted')
+      self.f_score = sk_metrics.f1_score(actual, predicted, average='weighted')
 
   @staticmethod
   def avg_score(metrics_arr):
@@ -82,13 +88,12 @@ class Metrics(O):
       precisions.append(metrics.precision)
       recalls.append(metrics.recall)
       f_scores.append(metrics.f_score)
-    score = O()
+    score = Metrics()
     score.accuracy = O(median=Metrics.median(accuracies), iqr=Metrics.iqr(accuracies))
     score.precision = O(median=Metrics.median(precisions), iqr=Metrics.iqr(precisions))
     score.recall = O(median=Metrics.median(recalls), iqr=Metrics.iqr(recalls))
     score.f_score = O(median=Metrics.median(f_scores), iqr=Metrics.iqr(f_scores))
     return score
-
 
   @staticmethod
   def iqr(x):
@@ -97,6 +102,13 @@ class Metrics(O):
   @staticmethod
   def median(x):
     return round(np.median(x), 2)
+
+  def __repr__(self):
+    return "Accuracy:%0.2f+-%0.2f; F Score:%0.2f+-%0.2f; Precision:%0.2f+-%0.2f; Recall:%0.2f+-%0.2f"\
+           % (self.accuracy.median, self.accuracy.iqr,
+              self.f_score.median, self.f_score.iqr,
+              self.precision.median, self.precision.iqr,
+              self.recall.median, self.recall.iqr)
 
 
 def get_papers_and_groups(miner):
@@ -114,62 +126,85 @@ def get_papers_and_groups(miner):
   return np.array(papers), np.array(groups)
 
 
-def ldade_linear_regression(*args):
+def pre_process_ldade(*args):
   x_train = args[0]
   y_train = args[1]
   x_test = args[2]
   y_test = args[3]
   x_train = [x.topics_score for x in x_train]
   x_test = [x.topics_score for x in x_test]
-  clf = LogisticRegression().fit(x_train, y_train)
-  predicted = clf.predict(x_test)
-  return predicted, Metrics(predicted, y_test)
+  return x_train, y_train, x_test, y_test
 
 
-def ldade_decision_tree(*args):
-  x_train = args[0]
-  y_train = args[1]
-  x_test = args[2]
-  y_test = args[3]
-  x_train = [x.topics_score for x in x_train]
-  x_test = [x.topics_score for x in x_test]
-  clf = DecisionTreeClassifier(random_state=RANDOM_STATE).fit(x_train, y_train)
-  predicted = clf.predict(x_test)
-  return predicted, Metrics(predicted, y_test)
-
-
-def vectorized_linear_regression(*args):
+def pre_process_tfidf(*args):
   x_train = args[0]
   y_train = args[1]
   x_test = args[2]
   y_test = args[3]
   x_train = [x.vectorized for x in x_train]
   x_test = [x.vectorized for x in x_test]
-  clf = LogisticRegression().fit(x_train, y_train)
-  predicted = clf.predict(x_test)
-  return predicted, Metrics(predicted, y_test)
+  return x_train, y_train, x_test, y_test
 
 
-def vectorized_decision_tree(*args):
+def pre_process_pruned_tfidf(*args):
   x_train = args[0]
   y_train = args[1]
   x_test = args[2]
   y_test = args[3]
   x_train = [x.vectorized for x in x_train]
   x_test = [x.vectorized for x in x_test]
+  k = K_BEST_RATE * len(x_train[0])
+  k_best = SelectKBest(chi2, k=k).fit(x_train, y_train)
+  return k_best.transform(x_train), y_train, k_best.transform(x_test), y_test
+
+
+def logistic_regression(pre_process, x_train, y_train, x_test, y_test):
+  x_train, y_train, x_test, y_test = pre_process(x_train, y_train, x_test, y_test)
+  clf = LogisticRegression().fit(x_train, y_train)
+  predicted = clf.predict(x_test)
+  return predicted, Metrics(predicted, y_test)
+
+
+def decision_tree(pre_process, x_train, y_train, x_test, y_test):
+  x_train, y_train, x_test, y_test = pre_process(x_train, y_train, x_test, y_test)
   clf = DecisionTreeClassifier(random_state=RANDOM_STATE).fit(x_train, y_train)
   predicted = clf.predict(x_test)
   return predicted, Metrics(predicted, y_test)
 
 
-def predict_conference(predictors, n_folds=10, n_topics=N_TOPICS, alpha=None, beta=None, n_iter=100,
+def svm(pre_process, x_train, y_train, x_test, y_test):
+  x_train, y_train, x_test, y_test = pre_process(x_train, y_train, x_test, y_test)
+  clf = LinearSVC(random_state=RANDOM_STATE).fit(x_train, y_train)
+  predicted = clf.predict(x_test)
+  return predicted, Metrics(predicted, y_test)
+
+
+def random_forest(pre_process, x_train, y_train, x_test, y_test):
+  x_train, y_train, x_test, y_test = pre_process(x_train, y_train, x_test, y_test)
+  clf = RandomForestClassifier(random_state=RANDOM_STATE).fit(x_train, y_train)
+  predicted = clf.predict(x_test)
+  return predicted, Metrics(predicted, y_test)
+
+
+def naive_bayes(pre_process, x_train, y_train, x_test, y_test):
+  x_train, y_train, x_test, y_test = pre_process(x_train, y_train, x_test, y_test)
+  clf = MultinomialNB().fit(x_train, y_train)
+  predicted = clf.predict(x_test)
+  return predicted, Metrics(predicted, y_test)
+
+
+def predict_conference(estimators, n_folds=10, n_topics=N_TOPICS, alpha=None, beta=None, n_iter=100,
                        min_tfidf_score=0.1, tfidf_top=100,
                        random_state=RANDOM_STATE):
+
+  def make_key(pred, pre_proc):
+    return "%s - %s" % (pred.__name__, pre_proc.__name__)
+
   graph = cite_graph(GRAPH_CSV)
   miner = Miner(graph)
   papers, groups = get_papers_and_groups(miner)
   skf = StratifiedKFold(n_splits=n_folds)
-  metrics_map = {predictor.__name__: [] for predictor in predictors}
+  metrics_map = {make_key(predictor, preprocessor): [] for predictor, preprocessor in estimators}
   index = 0
   for train_indices, test_indices in skf.split(papers, groups):
     index += 1
@@ -178,6 +213,7 @@ def predict_conference(predictors, n_folds=10, n_topics=N_TOPICS, alpha=None, be
     # Count Vectorizer
     train_papers = papers[train_indices]
     test_papers = papers[test_indices]
+    y_train, y_test = groups[train_indices], groups[test_indices]
     vectorizer = CountVectorizer(stop_words=STOP_WORDS, token_pattern=TOKEN_PATTERN)
     train_docs = [paper.raw for paper in train_papers]
     test_docs = [paper.raw for paper in test_papers]
@@ -211,17 +247,23 @@ def predict_conference(predictors, n_folds=10, n_topics=N_TOPICS, alpha=None, be
       sum_t = sum(topics)
       sum_t = sum_t if sum_t else 0.00001
       test_papers[i].topics_score = [t / sum_t for t in topics]
-    x_train, y_train, x_test, y_test = train_papers, groups[train_indices], test_papers, groups[test_indices]
-    for predictor in predictors:
-      print(predictor.__name__)
-      predicted, metrics = predictor(x_train, y_train, x_test, y_test)
-      metrics_map[predictor.__name__].append(metrics)
-  for predictor in predictors:
-    print (predictor.__name__)
-    measures = Metrics.avg_score(metrics_map[predictor.__name__])
+    x_train, x_test = train_papers, test_papers
+    for predictor, preprocessor in estimators:
+      key = make_key(predictor, preprocessor)
+      print(key)
+      predicted, metrics = predictor(preprocessor, x_train, y_train, x_test, y_test)
+      metrics_map[key].append(metrics)
+  for predictor, preprocessor in estimators:
+    key = make_key(predictor, preprocessor)
+    print ("### " + key)
+    measures = Metrics.avg_score(metrics_map[key])
     print (measures)
 
 
-predict_conference([vectorized_decision_tree, vectorized_linear_regression, ldade_decision_tree, ldade_linear_regression])
+predict_conference([(decision_tree, pre_process_tfidf), (decision_tree, pre_process_pruned_tfidf), (decision_tree, pre_process_ldade),
+                    (logistic_regression, pre_process_tfidf), (logistic_regression, pre_process_pruned_tfidf), (logistic_regression, pre_process_ldade),
+                    (svm, pre_process_tfidf), (svm, pre_process_pruned_tfidf), (svm, pre_process_ldade),
+                    (random_forest, pre_process_tfidf), (random_forest, pre_process_pruned_tfidf), (random_forest, pre_process_ldade),
+                    (naive_bayes, pre_process_tfidf), (naive_bayes, pre_process_pruned_tfidf), (decision_tree, pre_process_ldade)])
 
 
